5083ca5ede7749d68d783522a8b1f68916b39399
Whitten, Tom  (NBCUniversal, Orlando) <tom.whitten@universalorlando.com>
2019-03-06 07:58:38 -0500

    linting

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -14,6 +14,6 @@ export class ProductDescriptionComponent implements OnInit {
   ngOnInit() {
     this._productService
       .getAlbum(1)
-      .subscribe(response => (this.albumInfo = response));
+      .subscribe(response => this.albumInfo = response);
   }
 }
